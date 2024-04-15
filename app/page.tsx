28dc9e0ac24bec05bddc43476e69b66f053af923
Arthur Deruelle <arthur@MBP-de-Arthur.lan>
2024-04-15 19:27:20 +0200

    fix header issue logo dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import { auth } from "@clerk/nextjs";
-
 export default async function Home() {
-  const { userId } = auth();
-  console.log(userId);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
--- components/theme.tsx
+++ components/theme.tsx
@@ -3,13 +3,13 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/UI/button";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/UI/dropdown-menu";
+} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
