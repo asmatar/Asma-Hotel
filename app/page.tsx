768b7978f9ad8148eb3bbc86d97e908c55188b2a
Arthur Deruelle <arthur@MBP-de-Arthur.lan>
2024-04-17 17:42:44 +0200

    header menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import FramerDiv from "@/components/framer/div";
+/* import FramerDiv from "@/components/framer/div"; */
 export default async function Home() {
   return (
     <>
-      <FramerDiv></FramerDiv>
-
+      {/*  <FramerDiv></FramerDiv> */}
       <main className="flex min-h-full flex-col items-center justify-between p-24"></main>
     </>
   );
