568dbfc0dde02a9130e0633c6706e1a1c207bf57
Arthur Deruelle <arthur@MBP-de-Arthur.lan>
2024-04-17 17:01:14 +0200

    animation enter done

--- components/framer/div.tsx
+++ components/framer/div.tsx
@@ -1,66 +1,45 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { useAnimate } from "framer-motion";
+import { useEffect } from "react";
 const FramerDiv = ({ children }: { children: React.ReactNode }) => {
-  const containerVariants = {
-    hidden: { x: "-100vw" }, // initial prop
-    visible: {
-      x: "0",
-      transition: {
-        duration: 1.3,
-        delayChildren: 1.3,
-        staggerChildren: 0.5,
-      },
-    }, // animate pro
-  };
+  const [scope, animate] = useAnimate();
 
-  const variants = {
-    hidden: { y: 30, opacity: 0 },
-    visible: { y: 0, opacity: 1, transition: { duration: 1 } },
-  };
-
+  useEffect(() => {
+    const enterAnimation = async () => {
+      await animate(
+        scope.current,
+        { left: "0vw", opacity: 1 },
+        { duration: 1.3 },
+      );
+      await animate("h1", { y: -50, opacity: 1 }, { duration: 0.5 });
+      await animate(
+        "h2",
+        { y: -50, opacity: 1 },
+        { duration: 0.5, delay: 0.2 },
+      );
+      await animate("h3", { y: -50, opacity: 1 }, { duration: 0.5 });
+      await animate("h3", { y: 0, opacity: 0 }, { duration: 0.5, delay: 0.5 });
+      await animate("h2", { y: 0, opacity: 0 }, { duration: 0.5 });
+      await animate("h1", { y: 0, opacity: 0 }, { duration: 0.5 });
+      await animate(
+        scope.current,
+        { left: "-100vw", opacity: 1 },
+        { duration: 1.3, delay: 0.2 },
+      );
+    };
+    enterAnimation();
+  }, []);
   return (
-    <motion.div
-      /*       initial={{ x: "-100vw" }}
-      animate={{ x: "0" }}
-      exit={{ x: "-100vw", opacity: 0 }}
-      transition={{ duration: 1, ease: "easeInOut" }}
-      onAnimationComplete={() => controls.start()} */
-      /// <reference path="" />
-      initial="hidden"
-      animate="visible"
-      variants={containerVariants}
-      className="w-full h-screen absolute top-0 left-0 bg-gray-50 flex flex-col gap-10 z-10 tracking-tight"
+    <div
+      ref={scope}
+      className="w-full h-screen absolute top-0  bg-gray-50 flex -left-[100vw] flex-col gap-10 z-10 tracking-tight py-14"
     >
-      <motion.h1
-        variants={variants}
-        /*         initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeInOut", delay: 1.1 }} */
-        className="text-8xl font-bold"
-      >
-        WELCOME
-      </motion.h1>
-      <motion.h2
-        className="text-8xl font-bold"
-        variants={variants}
-        /*  initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeInOut", delay: 2 }} */
-      >
-        ASMA HOTEL
-      </motion.h2>
-      <motion.h3
-        variants={variants}
-        /*  initial={{ y: 50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, ease: "easeInOut", delay: 3 }} */
-        className="text-8xl font-bold "
-      >
-        BOOK YOUR BEST HOTEL
-      </motion.h3>
+      <h1 className="text-8xl font-bold opacity-0 ">WELCOME</h1>
+      <h2 className="text-8xl font-bold opacity-0">ASMA HOTEL</h2>
+      <h3 className="text-8xl font-bold opacity-0 ">BOOK YOUR BEST HOTEL</h3>
       {children}
-    </motion.div>
+    </div>
   );
 };
 
